a481b3e4d2ad758ea87e88c3ce4211d169e63e5b
groomit324 <gangho324@naver.com>
2022-12-16 14:26:31 +0900

    Initial commit

---
+++ src/main/java/hello/springStudyBasic/SpringStudyBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.springStudyBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springStudyBasic/SpringStudyBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springStudyBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
