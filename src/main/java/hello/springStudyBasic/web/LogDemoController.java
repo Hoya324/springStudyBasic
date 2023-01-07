a1aca8544788bf2f10ee9820de306659b5933df1
groomit324 <gangho324@naver.com>
2023-01-07 17:32:01 +0900

    2023.01.07

---
+++ src/main/java/hello/springStudyBasic/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.springStudyBasic.web;
+
+import hello.springStudyBasic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // 현재 화면이 없기 때문에 문자 그대로 출력함.
+    // HttpServletRequest - 고객 요청 정보 받을 수 있음
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
